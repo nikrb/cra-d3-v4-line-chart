c457f994eda04c81ef825de3b3eb2571dfd721bb
nikrb <nik_rb@yahoo.com>
2017-09-05 15:02:57 +0100

    add some data points

--- src/App.js
+++ src/App.js
@@ -11,10 +11,17 @@ class App extends Component {
       { date: "2017-08-30", close: "125"},
       { date: "2017-08-29", close: "276"},
       { date: "2017-08-28", close: "67"},
-      { date: "2017-08-27", close: "380"}
+      { date: "2017-08-27", close: "380"},
+      { date: "2017-08-26", close: "342"},
+      { date: "2017-08-25", close: "250"},
+      { date: "2017-08-24", close: "125"},
+      { date: "2017-08-23", close: "100"},
+      { date: "2017-08-22", close: "90"},
+      { date: "2017-08-21", close: "89"}
     ];
     return (
       <div className="App">
+        <h1>Line Chart</h1>
         <LineChart margin={margin}
           data={data} width={600} height={400} />
       </div>
